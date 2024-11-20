8aa365c22556327fa1399d71add206c96404d0e0
Omurbek <shaiurbekuuluomurbek@gmail.com>
2024-11-20 17:41:02 +0600

    commit

---
+++ src/main/java/omurbek/medcheck/MedCheckApplication.java
@@ -0,0 +1,13 @@
+package omurbek.medcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedCheckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/omurbek/medcheck/MedCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package omurbek.medcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
